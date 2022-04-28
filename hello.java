d594984cc11e70d754f9f5dbab300ff5b93ebe6e
Breezeeeeeeeeeee <104517575+Breezeeeeeeeeeee@users.noreply.github.com>
2022-04-28 10:36:09 +0800

    the first try of file

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class hash {
+    public static void main(String[] args) {
+
+        int a=5;
+        int b=3;
+        assert a!=b:"yes";//true继续执行,false报错并输出信息
+        System.out.println("last");
+
+}
+}
